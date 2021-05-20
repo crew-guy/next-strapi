e5019964870dd014d7cab28e33c9c3c75b0ddab7
Ankit <ankit.sanghavi87@gmail.com>
2021-05-20 16:41:31 +0530

    blog created

---
+++ blog-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1e8f0589b412b5c786d2f898bf560442'),
+    },
+  },
+});
---
+++ blog-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '96adbaff-97e4-4f34-9c63-bd46924bd7da'
+};
